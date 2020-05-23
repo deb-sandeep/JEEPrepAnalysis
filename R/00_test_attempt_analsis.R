1ea4bfcc86b5c1297e0a6af1e285c6aa4750fda7
Sandeep Deb <deb.sandeep@gmail.com>
2020-05-23 20:42:01 +0530

    first commit

---
+++ R/00_test_attempt_analsis.R
@@ -0,0 +1,8 @@
+project_dir <- "~/projects/source/JEEPrepAnalysis"
+
+wd <- list()
+wd$data <- paste0( project_dir, "/data" )
+wd$output <- paste0( project_dir, "/output" ) 
+
+# df <- read.csv( "~/temp/test_attempt_dataset.csv", header = TRUE )
+
---
+++ README.md
@@ -0,0 +1,1 @@
+## JEE Preparation Analysis
